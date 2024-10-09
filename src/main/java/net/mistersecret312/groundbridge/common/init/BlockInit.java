03761fb07234b958c55aa5f85685455b024a702d
mistersecret312 <sergiigamer7@gmail.com>
2024-10-09 13:07:18 +0300

    Energon generation.

--- src/main/java/net/mistersecret312/groundbridge/common/init/BlockInit.java
+++ src/main/java/net/mistersecret312/groundbridge/common/init/BlockInit.java
@@ -1,8 +1,11 @@
 package net.mistersecret312.groundbridge.common.init;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -18,7 +21,19 @@ public class BlockInit {
 
     public static final DeferredRegister<Block> BLOCKS = DeferredRegister.create(ForgeRegistries.BLOCKS, GroundBridge.MODID);
 
-    public static final RegistryObject<Block> ENERGON_CRYSTAL_SMALL = registerWithItem("energon_crystal_small", () -> new EnergonCrystalSmallBlock(BlockBehaviour.Properties.of().strength(15F, 15F).noOcclusion()));
+    public static final RegistryObject<Block> BLUE_ENERGON_ORE = registerWithItem("blue_energon_ore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.DIAMOND_BLOCK)),
+            properties -> properties.stacksTo(64)
+            .rarity(Rarity.create("blue_energon", ChatFormatting.BLUE)));
+    public static final RegistryObject<Block> RED_ENERGON_ORE = registerWithItem("red_energon_ore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.DIAMOND_BLOCK)),
+            properties -> properties.stacksTo(64)
+            .rarity(Rarity.create("red_energon", ChatFormatting.DARK_RED)));
+    public static final RegistryObject<Block> DARK_ENERGON_ORE = registerWithItem("dark_energon_ore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.DIAMOND_BLOCK)),
+            properties -> properties.stacksTo(64)
+            .rarity(Rarity.create("dark_energon", ChatFormatting.DARK_PURPLE)));
+    public static final RegistryObject<Block> TOXIC_ENERGON_ORE = registerWithItem("toxic_energon_ore", () -> new Block(BlockBehaviour.Properties.copy(Blocks.DIAMOND_BLOCK)),
+            properties -> properties.stacksTo(64)
+            .rarity(Rarity.create("toxic_energon", ChatFormatting.GREEN)));
+
 
     public static <T extends Block> RegistryObject<T> registerWithItem(String name, final Supplier<T> block, Function<Item.Properties, Item.Properties> properties){
         final RegistryObject<T> reg = BLOCKS.register(name, block);
--- src/main/java/net/mistersecret312/groundbridge/common/init/ItemInit.java
+++ src/main/java/net/mistersecret312/groundbridge/common/init/ItemInit.java
@@ -2,8 +2,10 @@ package net.mistersecret312.groundbridge.common.init;
 
 import java.util.function.Function;
 
+import net.minecraft.ChatFormatting;
 import net.minecraft.world.item.BlockItem;
 import net.minecraft.world.item.Item;
+import net.minecraft.world.item.Rarity;
 import net.minecraft.world.level.block.Block;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
@@ -14,6 +16,15 @@ import net.mistersecret312.groundbridge.GroundBridge;
 public class ItemInit {
 
     public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, GroundBridge.MODID);
+
+    public static final RegistryObject<Item> BLUE_ENERGON = ITEMS.register("energon", () -> new Item(new Item.Properties().stacksTo(64)
+            .rarity(Rarity.create("blue_energon", ChatFormatting.BLUE))));
+    public static final RegistryObject<Item> RED_ENERGON = ITEMS.register("red_energon", () -> new Item(new Item.Properties().stacksTo(64)
+            .rarity(Rarity.create("red_energon", ChatFormatting.DARK_RED))));
+    public static final RegistryObject<Item> DARK_ENERGON = ITEMS.register("dark_energon", () -> new Item(new Item.Properties().stacksTo(64)
+            .rarity(Rarity.create("dark_energon", ChatFormatting.DARK_PURPLE))));
+    public static final RegistryObject<Item> TOXIC_ENERGON = ITEMS.register("toxic_energon", () -> new Item(new Item.Properties().stacksTo(64)
+            .rarity(Rarity.create("toxic_energon", ChatFormatting.GREEN))));
 
     public static RegistryObject<BlockItem> registerBlockItem(RegistryObject<?extends Block> block, Function<Item.Properties, Item.Properties> prop){
         return ITEMS.register(block.getId().getPath(), () -> new BlockItem(block.get(), prop.apply(new Item.Properties())));
